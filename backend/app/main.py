655a11421d4301b3a388a3f3c5e98250a5833f7a
CAanil0019 <caanilkumarpanigrahi@gmail.com>
2026-05-15 19:49:46 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,35 +1,35 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import payment, charges, reports
+
+from app.routers import payment, charges, reports, auth
 from app.database import Base, engine
 from app import models
-from app.routers import auth
 from app.services.scheduler_service import start_scheduler
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="AFPL Payment Portal")
 
-start_scheduler()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
-        "https://annapurnafinance-charges.netlify.app"
+        "http://127.0.0.1:5173",
+        "https://annapurnafinance-charges.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+start_scheduler()
+
 app.include_router(payment.router)
 app.include_router(charges.router)
+app.include_router(reports.router)
+app.include_router(auth.router)
+
 
 @app.get("/")
 def home():
     return {"message": "AFPL Payment Portal Running"}
-
-app.include_router(reports.router)
-app.include_router(auth.router)
