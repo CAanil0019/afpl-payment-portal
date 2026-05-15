82080541bcbfa58a0783b0b3e17c04efb38a8f95
CAanil0019 <caanilkumarpanigrahi@gmail.com>
2026-05-15 19:59:02 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "https://annapurnafinance-charges.netlify.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
